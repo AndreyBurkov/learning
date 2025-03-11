729820fccaf23050168c7d97a18b2c87c4bdf29e
Andrey <burkov_andrey_alex@mail.ru>
2025-03-11 23:20:35 +0300

    some commit

--- src/main/java/ru/test/MainTest.java
+++ src/main/java/ru/test/MainTest.java
@@ -1,32 +1,20 @@
-import java.net.URI;
+package ru.test;
+
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 public class MainTest {
 
     public static void main(String[] args) throws URISyntaxException, InterruptedException {
-        URI uri = new MainTest().getClass().getClassLoader().getResource("test.txt").toURI();
+        Optional<String> optional = Optional.ofNullable("new");
+        Optional<String> qwe = optional.or(() -> {
+            System.out.println("qwe");
+            return Optional.of("\"qwe\"");
+        });
 
-
-        int[] arr = new int[3];
-        arr[0] = 10;
-        arr[1] = 20;
-        arr[2] = 30;
-
-        List<int[]> list = new ArrayList<>();
-        list.add(arr);
-
-        if (arr instanceof int[]) {
-            System.out.println("Class name: " + arr.getClass().getName());
-        }
-        foo(arr);
+        System.out.println(qwe.orElse("vvv"));
     }
-
-    private static void foo(Object o) {
-        System.out.println(o);
-    }
-
 
 }
 
--- src/test/java/ru/optional/MainOptionalTest.java
+++ src/test/java/ru/optional/MainOptionalTest.java
@@ -2,9 +2,12 @@ package ru.optional;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -72,4 +75,103 @@ class MainOptionalTest {
         System.out.println("Inside getMyDefault()");
         return "some default";
     }
+
+    @Test
+    public void whenOrElseThrowWorks_thenCorrect() {
+        final String nullName = null;
+        assertThrows(IllegalArgumentException.class,
+                () -> Optional.ofNullable(nullName).orElseThrow(IllegalArgumentException::new));
+    }
+
+    @Test
+    public void whenNoArgOrElseThrowWorks_thenCorrect() {
+        final String nullName = null;
+        assertThrows(NoSuchElementException.class,
+                () -> Optional.ofNullable(nullName).orElseThrow());
+    }
+
+    @Test
+    public void whenOptionalFilterWorks_thenCorrect() {
+        Integer year = 2022;
+        Optional<Integer> yearOptional = Optional.of(year);
+        boolean is2022 = yearOptional.filter(y -> y == 2022).isPresent();
+        assertTrue(is2022);
+        boolean is2021 = yearOptional.filter(y -> y == 2021).isPresent();
+        assertFalse(is2021);
+    }
+
+    @Test
+    public void whenFiltersWithoutOptional_thenCorrect() {
+        class Headphones {
+            private Double price;
+
+            public Headphones(Double price) {
+                this.price = price;
+            }
+
+            public Double getPrice() {
+                return price;
+            }
+
+            public void setPrice(Double price) {
+                this.price = price;
+            }
+
+            public static boolean priceIsInRange(Headphones headphones) {
+                return Optional.ofNullable(headphones)
+                        .map(Headphones::getPrice)
+                        .filter(p -> p >= 10)
+                        .filter(p -> p <= 15)
+                        .isPresent();
+            }
+        }
+
+        assertTrue(Headphones.priceIsInRange(new Headphones(10.0)));
+        assertFalse(Headphones.priceIsInRange(new Headphones(9.9)));
+        assertFalse(Headphones.priceIsInRange(new Headphones(null)));
+        assertFalse(Headphones.priceIsInRange(new Headphones(15.5)));
+        assertFalse(Headphones.priceIsInRange(null));
+    }
+
+    @Test
+    void test() {
+        Optional<String> optional1 = Optional.ofNullable(null);
+        Optional<String> optional2 = optional1.or(new Supplier<Optional<? extends String>>() {
+            @Override
+            public Optional<? extends String> get() {
+                return Optional.of("or value");
+            }
+        });
+        assertEquals("or value", optional2.get());
+
+        optional1.ifPresentOrElse(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("consumer");
+            }
+        }, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("runnable");
+            }
+        });
+
+        optional2.ifPresentOrElse(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("consumer");
+            }
+        }, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("runnable");
+            }
+        });
+
+        Set<String> set = optional1.stream().collect(Collectors.toSet());
+        System.out.println(set.size());
+        set = optional2.stream().collect(Collectors.toSet());
+        System.out.println(set.size());
+    }
+
 }
