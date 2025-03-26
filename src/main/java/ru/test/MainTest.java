fa7f2f37eb7e9c7f152572f7a8653b470797d37d
Andrey <burkov_andrey_alex@mail.ru>
2025-03-26 11:21:16 +0300

    some commit

--- src/main/java/ru/test/MainTest.java
+++ src/main/java/ru/test/MainTest.java
@@ -4,23 +4,65 @@ import java.net.URISyntaxException;
 
 public class MainTest {
 
-    public static void main(String[] args) throws URISyntaxException, InterruptedException {
-        int i = 0;
+    private static Integer MAIN_CONST;
 
-        for (; i <= 10; calculate(i)) {
-            System.out.println(i);
-            i+=2;
-        }
-
+    static {
+        System.out.println("Main static init");
+        MAIN_CONST = 67676;
     }
 
-    private static int calculate(int x) {
-        return x++;
+    public static void main(String[] args) throws URISyntaxException, InterruptedException, ClassNotFoundException {
+//        new B();
+        new B("asdf");
+//        System.out.println(MAIN_CONST);
     }
 
 }
 
+class A {
 
+    private static Integer MAIN_CONST;
+
+    static {
+        System.out.println("A static init");
+        MAIN_CONST = 67676;
+    }
+
+    private String value;
+
+//    public A() {
+//        System.out.println("Empty A constructor");
+//    }
+
+    public A(String value) {
+        System.out.println("Value A constructor");
+        this.value = value;
+    }
+
+}
+
+class B extends A {
+
+    private static Integer MAIN_CONST;
+
+    static {
+        System.out.println("B static init");
+        MAIN_CONST = 67676;
+    }
+
+    private String value;
+
+//    public B() {
+//        System.out.println("Empty B constructor");
+//    }
+
+    public B(String value) {
+//        super(value);
+        System.out.println("Value B constructor");
+        this.value = value;
+    }
+
+}
 
 
 
