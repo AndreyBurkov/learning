606c83a77d80dce4975ed1957124990f52ca4a22
Andrey <burkov_andrey_alex@mail.ru>
2025-04-01 18:30:59 +0300

    some commit

--- src/main/java/ru/test/MainTest.java
+++ src/main/java/ru/test/MainTest.java
@@ -1,8 +1,14 @@
 package ru.test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class MainTest {
 
     public static void main(String[] args) {
+
+
 
     }
 
