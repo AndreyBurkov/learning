baf873bf623f7d007798e7bcdb2068b442fd744f
Andrey <burkov_andrey_alex@mail.ru>
2025-04-02 21:30:22 +0300

    some commit

--- src/main/java/ru/test/MainTest.java
+++ src/main/java/ru/test/MainTest.java
@@ -1,14 +1,24 @@
 package ru.test;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MainTest {
 
     public static void main(String[] args) {
 
+        List<String> socks = List.of("red", "green", "blue", "pink", "blue", "red", "blue", "pink", "yellow");
 
+        List<String> result = socks.stream().collect(Collectors.groupingBy(String::toString)).entrySet().stream()
+                .filter(entry -> entry.getValue().size() % 2 != 0).map(Map.Entry::getKey).collect(Collectors.toList());
+
+        System.out.println(result);
+
+        long count = socks.stream().collect(Collectors.groupingBy(String::toString)).entrySet().stream()
+                .filter(entry -> entry.getValue().size() % 2 != 0).count();
+
+        System.out.println(count);
 
     }
 
