691ac0aa9df3f9a5359fd47b640dab4fa1002c51
Andrey <burkov_andrey_alex@mail.ru>
2025-04-10 23:52:31 +0300

    some commit

--- src/main/java/ru/test/MainTest.java
+++ src/main/java/ru/test/MainTest.java
@@ -2,48 +2,26 @@ package ru.test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class MainTest {
 
+    MainTest mainTest = new MainTest();
+
+    public int show() {
+        return true ? null : 0;
+    }
+
     public static void main(String[] args) {
-
-        List<String> socks = List.of("red", "green", "blue", "pink", "blue", "red", "blue", "pink", "yellow");
-
-        List<String> result = socks.stream().collect(Collectors.groupingBy(String::toString)).entrySet().stream()
-                .filter(entry -> entry.getValue().size() % 2 != 0).map(Map.Entry::getKey).collect(Collectors.toList());
-
-        System.out.println(result);
-
-        long count = socks.stream().collect(Collectors.groupingBy(String::toString)).entrySet().stream()
-                .filter(entry -> entry.getValue().size() % 2 != 0).count();
-
-        System.out.println(count);
-///////////////////////////////////////////////
-
-        final int SIZE = 100_000;
-
-        ArrayList<Integer> integers = new ArrayList<>(SIZE);
-        for (int i = 1; i <= SIZE; i++) {
-            integers.add(i);
-        }
-
-        long startTime = System.nanoTime();
-        long sum = integers.stream().mapToLong(value -> value).sum();
-        double elapsedTime = (System.nanoTime() - startTime) / 1_000_000_000d;
-        System.out.println("sum=" + sum + " ; elapsedTime=" + elapsedTime);
-
-
-        startTime = System.nanoTime();
-        sum = integers.stream().mapToLong(value -> value).parallel().sum();
-        elapsedTime = (System.nanoTime() - startTime) / 1_000_000_000d;
-        System.out.println("sum=" + sum + " ; elapsedTime=" + elapsedTime);
-
-
-//         sum = integers.stream().mapToInt(value -> value).parallel().sum();
+        MainTest mainTest = new MainTest();
+        mainTest.show();
 
 
     }
 
+
 }
+
+
+
+
+
