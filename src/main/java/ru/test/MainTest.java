fdda93cd9e684bf9b1d1212acd92b119c46fde63
Andrey <burkov_andrey_alex@mail.ru>
2025-04-16 09:03:03 +0300

    some commit

--- src/main/java/ru/test/MainTest.java
+++ src/main/java/ru/test/MainTest.java
@@ -1,21 +1,24 @@
 package ru.test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class MainTest {
 
-    MainTest mainTest = new MainTest();
-
-    public int show() {
-        return true ? null : 0;
-    }
 
     public static void main(String[] args) {
-        MainTest mainTest = new MainTest();
-        mainTest.show();
 
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
 
+        list.add(2, "X");
+        Iterator<String> iterator = list.iterator();
+        System.out.println(list);
     }
 
 
