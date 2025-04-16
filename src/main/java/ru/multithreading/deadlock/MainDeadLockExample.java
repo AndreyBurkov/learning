f979b01986f1d3bdaaf9a6efd540b18a0146242e
AndreyBurkov <burkov_andrey_alex@mail.ru>
2025-04-16 16:50:43 +0300

    some commit

--- src/main/java/ru/multithreading/deadlock/MainDeadLockExample.java
+++ src/main/java/ru/multithreading/deadlock/MainDeadLockExample.java
@@ -22,40 +22,42 @@ public class MainDeadLockExample implements Runnable {
         new MainDeadLockExample();
     }
 
-}
 
-class A {
 
-    public synchronized void runA(B b) {
-        System.out.println("A is running");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+    class A {
+
+        public synchronized void runA(B b) {
+            System.out.println("A is running");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            b.getB();
         }
-        b.getB();
-    }
 
-    public synchronized void getA() {
-        System.out.println("getA is triggered");
-    }
-
-}
-
-class B {
-
-    public synchronized void runB(A a) {
-        System.out.println("B is running");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        public synchronized void getA() {
+            System.out.println("getA is triggered");
         }
-        a.getA();
+
     }
 
-    public synchronized void getB() {
-        System.out.println("getB is triggered");
+    class B {
+
+        public synchronized void runB(A a) {
+            System.out.println("B is running");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            a.getA();
+        }
+
+        public synchronized void getB() {
+            System.out.println("getB is triggered");
+        }
+
     }
 
 }
